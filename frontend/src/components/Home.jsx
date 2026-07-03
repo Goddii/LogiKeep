558a6b02587f3c3181f75ea85c430cb597418c72
Goddii <otienogodwin23@gmail.com>
2026-07-03 12:07:12 +0300

    add inventorytoolbar and stats components and configure to home

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,38 @@
 import Navbar from "./Navbar"
+import Stats from "./Stats"
+import InventoryToolbar from "./InventoryToolbar"
+import { useEffect, useState } from "react"
 
 function Home() {
+
+    const [items, setItems] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+    const [activeFilter, setActiveFilter] = useState('ALL')
+    const [sortBy, setSortBy] = useState('name')
+
+
+
+
+    const totalCounts = {
+        all: items.length,
+        out: items.filter(i => i.stock === 0).length,
+        low: items.filter(i => i.stock > 0 && i.stock < 15).length,
+        ok: items.filter(i => i.stock >= 15).length,
+    }
     return(
         <div>
             <Navbar />
+            <br />
+            <Stats />
+            <br />
+            <InventoryToolbar 
+                searchTerm={searchTerm}
+                setSearchTerm={setSearchTerm}
+                activeFilter={activeFilter}
+                sortBy={sortBy}
+                setSortBy={setSortBy}
+                totalCounts={totalCounts}
+            />
         </div>
     )
 }
